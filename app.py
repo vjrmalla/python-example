5a71156b849796a1f3a6b4e3fcb909ef6545949c
Venkat <venkat@test.com>
2021-05-04 10:44:49 +0100

    first python project

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def hello():
+    return "hello world"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
